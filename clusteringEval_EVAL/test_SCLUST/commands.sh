fbd6973b46142445c529329940b40d0381551b03
chilpert2 <cecile.hilpert@gmail.com>
2019-01-30 14:04:33 +0100

    update preprocessing steps in commands.sh

--- clusteringEval_EVAL/test_SCLUST/commands.sh
+++ clusteringEval_EVAL/test_SCLUST/commands.sh
@@ -8,15 +8,10 @@ tar -xvf dataset.tar.gz
 
 for i in 01 02 03 04 05 06 07 08 09 10; do 
 	gunzip clusteringEval_DATA/sample$i\-1000sp-Powerlaw.fastq.gz 
-	#Remove chimeras
-	python3 bin/exclude_chimeras.py clusteringEval_DATA/sample$i\-1000sp-Powerlaw.fastq sample$i\-1000sp-Powerlaw.noChimeras.fastq	
-	#Dereplication
-	vsearch --derep_fulllength clusteringEval_DATA/sample$i\-1000sp-Powerlaw.noChimeras.fastq --output clusteringEval_DATA/sample$i\-1000sp-Powerlaw.noChimeras.derep.fasta --threads 8 --sizeout --uc clusteringEval_DATA/sample$i\-1000sp-Powerlaw.noChimeras.derep.uc
-	#Taxonomy treatment 
-	grep "reference=" clusteringEval_DATA/sample$i\-20sp-Powerlaw.noChimeras.fastq > clusteringEval_DATA/sample$i\-20sp-Powerlaw.noChimeras.fastq.headers
-	python3 bin/frogs_taxo.py clusteringEval_DATA/sample$i\-1000sp-Powerlaw.noChimeras.fastq.headers clusteringEval_DATA/sample$i\-1000sp-Powerlaw.noChimeras.derep.fasta clusteringEval_DATA/sample$i\-1000sp-Powerlaw.noChimeras.derep.abundance.tsv clusteringEval_DATA/sample$i\-1000sp-Powerlaw.noChimeras.derep.taxo.tsv 
-	
+	bash bin/preprocessing.sh clusteringEval_DATA/sample$i\-1000sp-Powerlaw.fastq
 done 
+
+bash bin/combined_files.sh clusteringEval_DATA/*-1000sp-*.preprocessing_stats.tsv > clusteringEval_DATA/all_samples-1000sp-Powerlaw.preprocessing_stats.tsv
 
 #2. Clustering and evaluation 
 
