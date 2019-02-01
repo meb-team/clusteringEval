b0e3cbb5a98b316345d836ec5516d739d4f5768c
chilpert2 <cecile.hilpert@gmail.com>
2019-02-01 15:42:19 +0100

    write figures to svg, replace old figures

--- bin/testSclust_graphs.R
+++ bin/testSclust_graphs.R
@@ -1,69 +1,40 @@
 library(ggplot2)  
+library(gridExtra) 
 
 f=read.table("clusteringEval_TEST_SCLUST/testSclust.eval.tsv",header=TRUE,sep="\t") 
 f$quality=factor(f$quality) 
 levels(f$sample)=c("Sample01","Sample02","Sample03","Sample04","Sample05","Sample06","Sample07","Sample08","Sample09","Sample10")
+levels(f$algo)=c("Accurate","Default") 
 f$p_singletons=f$singletons/f$total_clusters*100
 
-ari_all=ggplot(f,aes(x=threshold.d,y=ARI,color=quality,linetype=algo))+geom_point()+geom_line()+facet_wrap(~sample)+labs(color="Quality",x="Clustering threshold (%)",y="Adjusted Rand Index",linetype="Algo")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
+f_recall=data.frame(tool=f$tool,sample=f$sample,value=f$recall,type="Recall",threshold=f$threshold.d,algo=f$algo,quality=f$quality)
+f_precision=data.frame(tool=f$tool,sample=f$sample,value=f$precision,type="Precision",threshold=f$threshold.d,algo=f$algo,quality=f$quality)
+f_recall_precision=rbind(f_recall,f_precision)
 
-recall_all=ggplot(f,aes(x=threshold.d,y=recall,color=quality,linetype=algo))+geom_point()+geom_line()+facet_wrap(~sample)+labs(color="Quality",x="Clustering threshold (%)",y="Recall",linetype="Algo")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
+ari_all=ggplot(f,aes(x=threshold.d,y=ARI,color=quality,linetype=algo))+geom_point()+geom_line()+facet_wrap(~sample,ncol=5)+labs(color="Quality",x="Clustering threshold (%)",y="Adjusted Rand Index",linetype="Mode")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
 
-precision_all=ggplot(f,aes(x=threshold.d,y=precision,color=quality,linetype=algo))+geom_point()+geom_line()+facet_wrap(~sample)+labs(color="Quality",x="Clustering threshold (%)",y="Precision",linetype="Algo")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
+recall_all=ggplot(f,aes(x=threshold.d,y=recall,color=quality,linetype=algo))+geom_point()+geom_line()+facet_wrap(~sample,ncol=5)+labs(title="Recall",color="Quality",x="Clustering threshold (%)",y="Recall",linetype="Mode")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16),plot.title=element_text(size=20))
 
-singletons_all=ggplot(f,aes(x=threshold.d,y=p_singletons,color=quality,linetype=algo))+geom_point()+geom_line()+facet_wrap(~sample)+labs(color="Quality",x="Clustering threshold (%)",y="Singletons (%)",linetype="Algo")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
+precision_all=ggplot(f,aes(x=threshold.d,y=precision,color=quality,linetype=algo))+geom_point()+geom_line()+facet_wrap(~sample,ncol=5)+labs(title="Precision",color="Quality",x="Clustering threshold (%)",y="Precision",linetype="Mode")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16),plot.title=element_text(size=20))
+
+singletons_all=ggplot(f,aes(x=threshold.d,y=p_singletons,color=quality,linetype=algo))+geom_point()+geom_line()+facet_wrap(~sample,ncol=5)+labs(color="Quality",x="Clustering threshold (%)",y="Singletons (%)",linetype="Algo")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
 
 f$threshold.d=factor(f$threshold.d)
-levels(f$algo)=c("Accurate","Default") 
 
 ari_boxplot=ggplot(f,aes(x=threshold.d,y=ARI,fill=quality))+geom_boxplot()+facet_wrap(~algo,ncol=1)+labs(fill="Quality",x="Clustering threshold (%)",y="Ajusted Rand Index")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
 
-recall_boxplot=ggplot(f,aes(x=threshold.d,y=recall,fill=quality))+geom_boxplot()+facet_wrap(~algo,ncol=1)+labs(fill="Quality",x="Clustering threshold (%)",y="Recall")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
-
-precision_boxplot=ggplot(f,aes(x=threshold.d,y=precision,fill=quality))+geom_boxplot()+facet_wrap(~algo,ncol=1)+labs(fill="Quality",x="Clustering threshold (%)",y="Precision")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
+recall_precision_boxplot=ggplot(f_recall_precision,aes(x=threshold,y=value,fill=quality))+geom_boxplot()+facet_grid(algo~type)+labs(fill="Quality",x="Clustering threshold (%)",y="Value")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16),strip.text.y=element_text(size=16))
 
 singletons_boxplot=ggplot(f,aes(x=threshold.d,y=p_singletons,fill=quality))+geom_boxplot()+facet_wrap(~algo,ncol=1)+labs(fill="Quality",x="Clustering threshold (%)",y="Singletons (%)")+theme(axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),legend.title=element_text(size=16),legend.text=element_text(size=14),strip.text.x=element_text(size=16))
 
+ggsave(file="clusteringEval_RESULTS/test_SCLUST/ari_all.svg",plot=ari_all,width=12)
+recall_precision_all=grid.arrange(recall_all,precision_all)
+ggsave(file="clusteringEval_RESULTS/test_SCLUST/recall_precision_all.svg",plot=recall_precision_all,width=12,height=12)
+ggsave(file="clusteringEval_RESULTS/test_SCLUST/singletons_all.svg",plot=singletons_all,width=12)
 
-pdf("clusteringEval_RESULTS/test_SCLUST/ari_all.pdf",width=12)
-ari_all 
-dev.off() 
-
-pdf("clusteringEval_RESULTS/test_SCLUST/recall_all.pdf",width=12)
-recall_all 
-dev.off()
-
-pdf("clusteringEval_RESULTS/test_SCLUST/precision_all.pdf",width=12)
-precision_all 
-dev.off()
-
-pdf("clusteringEval_RESULTS/test_SCLUST/singletons_all.pdf",width=12)
-singletons_all 
-dev.off()
-
-pdf("clusteringEval_RESULTS/test_SCLUST/ari_boxplot.pdf",height=10)
-ari_boxplot 
-dev.off() 
-
-png("clusteringEval_RESULTS/test_SCLUST/ari_boxplot.png",width=800,height=800)
-ari_boxplot
-dev.off() 
-
-pdf("clusteringEval_RESULTS/test_SCLUST/recall_boxplot.pdf")
-recall_boxplot 
-dev.off()
-
-pdf("clusteringEval_RESULTS/test_SCLUST/precision_boxplot.pdf")
-precision_boxplot 
-dev.off()
-
-pdf("clusteringEval_RESULTS/test_SCLUST/singletons_boxplot.pdf")
-singletons_boxplot 
-dev.off()
-
-png("clusteringEval_RESULTS/test_SCLUST/singletons_boxplot.png",width=800,height=800)
-singletons_boxplot
-dev.off() 
+ggsave(file="clusteringEval_RESULTS/test_SCLUST/ari_boxplot.svg",plot=ari_boxplot,height=8)
+ggsave(file="clusteringEval_RESULTS/test_SCLUST/recall_precision_boxplot.svg",plot=recall_precision_boxplot) 
+ggsave(file="clusteringEval_RESULTS/test_SCLUST/singletons_boxplot.svg",plot=singletons_boxplot,height=8) 
 
 save.image(file="clusteringEval_RESULTS/test_SCLUST/test_SCLUST.Rdata") 
 
